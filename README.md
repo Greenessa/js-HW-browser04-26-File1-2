1de713d77d2f6f5fb74b4a52e19f5adacf0cd040
Margo <romanova184@yandex.ru>
2026-04-07 15:23:21 +0300

    Next commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/Greenessa/js-HW-browser04-26-File1-2)](https://ci.appveyor.com/api/projects/status/github/Greenessa/js-HW-browser04-26-File1-2)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/5kjvncr306n2pykc?svg=true)](https://ci.appveyor.com/project/Greenessa/js-hw-browser04-26-file1-2)
--- src/js/saveInStorage.js
+++ src/js/saveInStorage.js
@@ -1,23 +1,21 @@
 export default class SaveInStorage {
-    constructor(storage) {
-        this.storage = storage;
-    }
+  constructor(storage) {
+    this.storage = storage;
+  }
 
-    setInStorage (obj) {
-        this.storage.setItem('currentState', JSON.stringify(obj))
-    }
+  setInStorage(obj) {
+    this.storage.setItem("currentState", JSON.stringify(obj));
+  }
 
-    getFromStorage () {
-        try {
-            return JSON.parse(this.storage.getItem('currentState'));
-        } catch (error) {
-            throw new Error('Invalid state');
-        }
+  getFromStorage() {
+    try {
+      return JSON.parse(this.storage.getItem("currentState"));
+    } catch {
+      throw new Error("Invalid state");
     }
+  }
 
-    clearStorage () {
-        this.storage.clear('currentState');
-    }
+  clearStorage() {
+    this.storage.clear("currentState");
+  }
 }
-
-
